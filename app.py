7f0e1b31dd2c67edcbd816f69719a5c95ed67557
harshitamahajan2 <harshitamahajan42@gmail.com>
2024-10-22 23:37:59 +0530

    Final tweaks before deployment

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-
-from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+'''
+from flask import Flask, render_template, request, redirect  # type: ignore
+from flask_sqlalchemy import SQLAlchemy # type: ignore #
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///C:/Users/lenovo/Desktop/f pro/todo.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -57,8 +58,76 @@ def delete(sno):
     db.session.delete(todo)
     db.session.commit()
     return redirect('/')
-    
+@app.route('/')
+def home():
+    return "Hello, Render!"
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True,host='0.0.0.0' ,port= 8000)
+'''
+
+from flask import Flask, render_template, request, redirect  # type: ignore
+from flask_sqlalchemy import SQLAlchemy  # type: ignore
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+    allTodo = Todo.query.all()
+    return render_template('index.html', allTodo=allTodo)  # type: ignore # Ensure 'index.html' is in the 'templates' folder
+
+@app.route('/show')
+def products():
+    allTodo = Todo.query.all()
+    print(allTodo)
+    return 'this is products page'
+
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    if request.method == "POST":
+        title = request.form['title']
+        desc = request.form['desc']
+        todo.title = title
+        todo.desc = desc
+        db.session.commit()
+        return redirect('/')
+    return render_template('update.html', todo=todo)
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()  # Corrected to filter by 'sno'
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/')
+def home():
+    return "Hello, Render!"
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, host='0.0.0.0', port=8000)
